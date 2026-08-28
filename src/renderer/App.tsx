5870938e77e698e8fda0dec6ddfe193c9cc14764
mhassanakbar <mhassanakbar@outlook.com>
2026-08-28 15:18:20 +0500

    chore: setup react

---
+++ src/renderer/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h1>Welcome to Dev Mux</h1>
+      <p>Electron + Vite + React + TypeScript is officially up and running!</p>
+    </div>
+  );
+}
